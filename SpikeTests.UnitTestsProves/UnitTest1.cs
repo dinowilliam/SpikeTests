b0e26ee8637b0c47f5e2098e75b5924533c52c37
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2022-08-04 21:17:58 -0300

    Tests of the message

--- SpikeTests.UnitTestsProves/UnitTest1.cs
+++ SpikeTests.UnitTestsProves/UnitTest1.cs
@@ -9,17 +9,17 @@ namespace SpikeTests.UnitTestsProves {
 
         private SqlException GetSqlException() {
             SqlErrorCollection collection = Construct<SqlErrorCollection>();
-            SqlError error = Construct<SqlError>(-2, (byte)2, (byte)3, "server name", "error message", "proc", 100, (uint)1, new Exception());            
+            SqlError error = Construct<SqlError>(-2, (byte)2, (byte)3, "Servername", "Error Message For SQLServer Test", "proc", 100, (uint)1, new Exception());            
 
             typeof(SqlErrorCollection)
                 .GetMethod("Add", BindingFlags.NonPublic | BindingFlags.Instance)
                 .Invoke(collection, new object[] { error });
 
-            var e = typeof(SqlException)
+            var sqlException = typeof(SqlException)
                 .GetMethod("CreateException", BindingFlags.NonPublic | BindingFlags.Static, null, CallingConventions.ExplicitThis, new[] { typeof(SqlErrorCollection), typeof(string) }, new ParameterModifier[] { })
                 .Invoke(null, new object[] { collection, "11.0.0" }) as SqlException;
 
-            return e;
+            return sqlException;
         }
 
         private T Construct<T>(params object[] p) {

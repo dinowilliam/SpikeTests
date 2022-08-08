60025cf533a5a7a6ece8b403c5e7d755cc51972f
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2022-08-08 19:45:48 -0300

    Add Exceptions Types

--- SpikeTests.Infra.Support/CircuitBreaker/CircuitBreaker.cs
+++ SpikeTests.Infra.Support/CircuitBreaker/CircuitBreaker.cs
@@ -15,8 +15,55 @@ namespace SpikeTests.Infra.Support.CircuitBreaker {
         public void ExecuteAction(Action action) {
 
             if (IsOpen) {
-                // The circuit breaker is Open.
-                //... (see code sample below for details)
+                // The circuit breaker is Open. Check if the Open timeout has expired.
+                // If it has, set the state to HalfOpen. Another approach might be to
+                // check for the HalfOpen state that had be set by some other operation.
+                if (stateStore.LastStateChangedDateUtc + OpenToHalfOpenWaitTime < DateTime.UtcNow) {
+                    // The Open timeout has expired. Allow one operation to execute. Note that, in
+                    // this example, the circuit breaker is set to HalfOpen after being
+                    // in the Open state for some period of time. An alternative would be to set
+                    // this using some other approach such as a timer, test method, manually, and
+                    // so on, and check the state here to determine how to handle execution
+                    // of the action.
+                    // Limit the number of threads to be executed when the breaker is HalfOpen.
+                    // An alternative would be to use a more complex approach to determine which
+                    // threads or how many are allowed to execute, or to execute a simple test
+                    // method instead.
+                    bool lockTaken = false;
+                    try {
+                        Monitor.TryEnter(halfOpenSyncObject, ref lockTaken);
+                        if (lockTaken) {
+                            // Set the circuit breaker state to HalfOpen.
+                            stateStore.HalfOpen();
+
+                            // Attempt the operation.
+                            action();
+
+                            // If this action succeeds, reset the state and allow other operations.
+                            // In reality, instead of immediately returning to the Closed state, a counter
+                            // here would record the number of successful operations and return the
+                            // circuit breaker to the Closed state only after a specified number succeed.
+                            this.stateStore.Reset();
+                            return;
+                        }
+                    }
+                    catch (Exception ex) {
+                        // If there's still an exception, trip the breaker again immediately.
+                        this.stateStore.Trip(ex);
+
+                        // Throw the exception so that the caller knows which exception occurred.
+                        throw;
+                    }
+                    finally {
+                        if (lockTaken) {
+                            Monitor.Exit(halfOpenSyncObject);
+                        }
+                    }
+                }
+                // The Open timeout hasn't yet expired. Throw a CircuitBreakerOpen exception to
+                // inform the caller that the call was not actually attempted,
+                // and return the most recent exception received.
+                throw new CircuitBreakerOpenException(stateStore.LastException);
             }
 
             // The circuit breaker is Closed, execute the action.
---
+++ SpikeTests.Infra.Support/CircuitBreaker/Exceptions/CircuitBreakerOpenException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace SpikeTests.Infra.Support.CircuitBreaker.Exceptions {
+    [Serializable]
+    internal class CircuitBreakerOpenException : Exception {
+        private Exception lastException;
+
+        public CircuitBreakerOpenException() {
+        }
+
+        public CircuitBreakerOpenException(Exception lastException) {
+            this.lastException = lastException;
+        }
+
+        public CircuitBreakerOpenException(string? message) : base(message) {
+        }
+
+        public CircuitBreakerOpenException(string? message, Exception? innerException) : base(message, innerException) {
+        }
+
+        protected CircuitBreakerOpenException(SerializationInfo info, StreamingContext context) : base(info, context) {
+        }
+    }
+}

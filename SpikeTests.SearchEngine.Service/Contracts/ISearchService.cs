d3c85a9803e5e19fb7127d67c15dc5e2038ac11d
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2024-11-12 16:35:38 -0300

    Readd the SearchService

--- SpikeTests.SearchEngine.Service/Contracts/ISearchService.cs
+++ SpikeTests.SearchEngine.Service/Contracts/ISearchService.cs
@@ -1,12 +1,17 @@
-﻿using SpikeTests.SearchEngine.Service.DTOs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace SpikeTests.SearchEngine.Service.Contracts {
-    
-    public interface ISearchService {
-        
-        IEnumerable<SearchResultDTO> GetSearch();
 
+    using SpikeTests.SearchEngine.Domain.Entities;
+
+    public interface ISearchService {
+        public int Save(LogMessage log);                
+        public int Delete(Guid id);
+        public IEnumerable<LogMessage> GetAll();
+        public LogMessage GetById(Guid id);
+        public IEnumerable<LogMessage> LogFilter(LogSearch logSearch);
+        public int Upload(byte[] file);
+        public DateTime ParseDateTimeLinq(string dateToParse);        
     }
 }
